5ea649890146bf1e31e98e4b54ba5e0379213632
Ryo Kanbayashi <ryo.contact@gmail.com>
2018-02-09 12:25:47 +0900

    hitomazu wrote all

--- dnn_denose1.py
+++ dnn_denose1.py
@@ -14,11 +14,12 @@ from six.moves import xrange, zip
 import scipy.special as spc
 
 
-from keras.layers import Input, Dense
+from keras.layers import Dense
 from keras.models import Model, Sequential
 import os.path
 
-def uniting_channles(leftsignal, rightsignal):                                                                                                                                                               ret = []
+def uniting_channles(leftsignal, rightsignal):
+    ret = []
     for i, j in zip(leftsignal, rightsignal):
         ret.append(i)
         ret.append(j)
@@ -30,28 +31,28 @@ def train(train_in, train_out, test_in, test_out):
     input_len = 120
     hidden_dim = 300
 
-    model.add(Dence(input_len, activation='relu', input_shape=(input_len,1)))
-    model.add(Dence(hidden_dim, activation='relu'))
-    model.add(Dence(input_len, activation='relu'))
+    model.add(Dense(input_len, activation='relu', input_shape=(input_len)))
+    model.add(Dense(hidden_dim, activation='relu'))
+    model.add(Dense(input_len, activation='relu'))
 
     model.compile(optimizer='adam', loss='binary_crossentropy')
 
     #autoencoder.save_weights('autoencoder.h5')
     #autoencoder.load_weights('autoencoder.h5')
 
-    q, mod = divmode(len(train_in),input_len)
+    q, mod = divmod(len(train_in),input_len)
     train_in = train_in[0:len(train_in) - mod]
     x_train_in = np.reshape(train_in, (q, input_len))
 
-    q, mod = divmode(len(train_out),input_len)
+    q, mod = divmod(len(train_out),input_len)
     train_out = train_out[0:len(train_out) - mod]
     x_train_out = np.reshape(train_out, (q, input_len))
 
-    q, mod = divmode(len(test_in),input_len)
-    test_in = test_in[0:len(test_inl) - mod]
+    q, mod = divmod(len(test_in),input_len)
+    test_in = test_in[0:len(test_in) - mod]
     y_test_in = np.reshape(test_in, (q, input_len))
 
-    q, mod = divmode(len(test_out),input_len)
+    q, mod = divmod(len(test_out),input_len)
     test_out = train_out[0:len(test_out) - mod]
     y_test_out = np.reshape(test_out, (q, input_len))
 
@@ -62,7 +63,7 @@ def train(train_in, train_out, test_in, test_out):
                 shuffle=True,
                 validation_data=(y_test_in, y_test_out))
 
-   return model
+    return model
 
 
 def write(param,signal):
@@ -106,7 +107,7 @@ def denoise(signal, model):
 
     input_len = 120
 
-    q, mod = divmode(len(signal),input_len)
+    q, mod = divmod(len(signal),input_len)
     input = signal[0:len(signal) - mod]
     input = np.reshape(input, (q, input_len))
     
@@ -140,11 +141,21 @@ if __name__ == '__main__':
     l_output_train,r_output_train = separate_channels(train_output_signal)
     l_input_test,r_input_test = separate_channels(test_input_signal)
     l_output_test,r_output_test = separate_channels(test_output_signal)
+    
+    l_input_train = l_input_train[0:1000]
+    l_output_train = l_output_train[0:1000]
+    l_input_test = l_input_test[0:1000]
+    l_output_test = l_output_test[0:1000]
 
-    l_input_train_ = prerocess(l_input_train)
+    l_input_train_ = preprocess(l_input_train)
     l_output_train_ = preprocess(l_output_train)
     l_input_test_ = preprocess(l_input_test)
     l_output_test_ = preprocess(l_output_test)
     
+    print("preprocess finished.")
+
     model = train(l_input_train_, l_output_train_, l_input_test_, l_output_test_)
-    write(test_input_params, uniting_channles(denoise(l_input_test, model),denoise(r_input_test, model))
+
+    print("train finished.")
+
+    write(test_input_params, uniting_channles(denoise(l_input_test, model),denoise(r_input_test, model)))
